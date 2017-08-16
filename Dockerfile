fa902515c25cef1cbfcd500ef5945750caad8f73
cmchaol <cmchaol@users.noreply.github.com>
2017-08-16 16:09:56 +0800

    from v22, xorg-server, kernel

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM c5766/giid:v19
+FROM c5766/giid:v22
 # FROM gentoo/stage3-amd64-hardened
 
 ADD build.sh /
--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
 
+echo ">=sys-kernel/hardened-sources-4.8.17-r2:4.8.17-r2" > /etc/portage/package.mask/hardened-sources
+
+echo "app-editors/emacs xft X jpeg png svg tiff alsa gif imagemagick sound" > /etc/portage/package.use/emacs
+
 emerge \
-       x11-base/xorg-drivers
+       x11-base/xorg-server \
+       x11-apps/xrandr \
+       x11-terms/xterm \
+       x11-wm/spectrwm \
+       sys-kernel/hardened-sources \
+       app-editors/emacs \
+       --autounmask-write 
+
+yes | etc-update --automode -3
+
+emerge \
+       x11-base/xorg-drivers \
+       x11-base/xorg-server \
+       x11-apps/xrandr \
+       x11-terms/xterm \
+       x11-wm/spectrwm \
+       sys-kernel/hardened-sources \
+       app-editors/emacs
