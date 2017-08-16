a10965fb98069dba83e7ae7d609f1166b3c7f44c
cmchaol <cmchaol@users.noreply.github.com>
2017-08-17 07:39:31 +0800

    emacs, from v24

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 
-echo ">=sys-kernel/hardened-sources-4.8.17-r2:4.8.17-r2" > /etc/portage/package.mask/hardened-sources
-
-# echo "app-editors/emacs xft X jpeg png svg tiff alsa gif imagemagick sound" > /etc/portage/package.use/emacs
+echo "app-editors/emacs xft X jpeg png svg tiff alsa gif imagemagick" > /etc/portage/package.use/emacs
 
 emerge \
-       x11-base/xorg-server \
-       x11-apps/xrandr \
-       x11-terms/xterm \
-       x11-wm/spectrwm \
-       sys-kernel/hardened-sources
+       app-editors/emacs \
+       --autounmask-write 
+
+yes | etc-update --automode -3
+
+emerge \
+       app-editors/emacs
