6e368dd981d379e78a8dde34dd314d41dd561651
cmchaol <cmchaol@users.noreply.github.com>
2017-08-17 09:26:37 +0800

    from v24, proxychains, dhcpcd, sutossh, keychain, grub, mc, git

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 
-echo "app-editors/emacs xft X jpeg png svg tiff alsa gif imagemagick" > /etc/portage/package.use/emacs
-
 emerge \
-       app-editors/emacs \
-       --autounmask-write 
-
-yes | etc-update --automode -3
-
-emerge \
-       app-editors/emacs
+       net-misc/proxychains \
+       net-misc/dhcpcd \
+       net-misc/autossh \
+       net-misc/keychain \
+       sys-boot/grub \
+       app-misc/mc \
+       dev-vcs/git
