9d9e1714236b2a8322a90459330beaf1025e9b81
cmchaol <cmchaol@users.noreply.github.com>
2017-08-18 12:57:31 +0800

    compile kernel

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 
-emerge \
-       net-misc/proxychains \
-       net-misc/dhcpcd \
-       net-misc/autossh \
-       net-misc/keychain \
-       sys-boot/grub \
-       app-misc/mc \
-       dev-vcs/git
+f="https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker448-20170727-1425"; \
+wget -O /usr/src/linux/.config $f; \
+cd /usr/src/linux; \
+make && make modules_install; \
+KERNELVER=4.7.10-hardened; \
+EXTENSION=20170818; \
+cp .config /boot/config-${KERNELVER}-${EXTENSION}; \
+cp System.map /boot/System.map-${KERNELVER}-${EXTENSION}; \
+cp arch/x86_64/boot/bzImage /boot/kernel-${KERNELVER}-${EXTENSION}; \
+cp -a .config ../${KERNELVER}-${EXTENSION}.config.bk; \
