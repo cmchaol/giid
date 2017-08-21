ab8ecebfaacfc581cf4a77da9ea9b91e8e6d46c6
cmchaol <cmchaol@users.noreply.github.com>
2017-08-21 17:22:55 +0800

    emerge --sync, only

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
 
-echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
-
 emerge --sync
-
-echo ">=sys-kernel/hardened-sources-4.8.17-r2:4.8.17-r2" > /etc/portage/package.mask/hardened-sources
-
-emerge \
-       sys-kernel/hardened-sources
