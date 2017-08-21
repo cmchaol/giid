888bf6ee6b16fff156f8d0a04f752ce09749bede
cmchaol <cmchaol@users.noreply.github.com>
2017-08-22 07:48:50 +0800

    emerge grub

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 
-mkdir /dev/tmp
-
-echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
-
-echo ">=sys-kernel/hardened-sources-4.8.17-r2:4.8.17-r2" > /etc/portage/package.mask/hardened-sources
-
 emerge \
-       sys-kernel/hardened-sources
+       sys-boot/grub \
