a1413c0743dd5ed865ddf10a2d9d9b8b531c6d6e
cmchaol <cmchaol@users.noreply.github.com>
2017-08-21 16:46:16 +0800

    /etc/portage/make.profile is not a symlink and will probably prevent most merges

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ EOF
 
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 
-emerge-webrsync
+# emerge-webrsync
+
+emerge --sync
 
 echo ">=sys-kernel/hardened-sources-4.8.17-r2:4.8.17-r2" > /etc/portage/package.mask/hardened-sources
 
