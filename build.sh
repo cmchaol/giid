49bb9681ac93346ccddaa6a84f09b10d648f67ed
cmchaol <cmchaol@users.noreply.github.com>
2017-08-22 12:12:03 +0800

    cp .config compile

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 
+# PORTAGE_TMPDIR
 mkdir /dev/tmp
 
-emerge \
-       sys-boot/grub \
+
+sed -i 's/USE="/USE="udev /g' /etc/portage/make.conf 
+
+emerge --changed-use --deep @world 
+
+rc-update add udev sysinit
+
+
+f=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker4710-sm-cp-tm-fu-ev-mu-us-fs-ne-ud-x-go-20170822.config
+
+cd /usr/src/linux
+
+wget -O .config $f
+
+time \
+make  && make modules_install
