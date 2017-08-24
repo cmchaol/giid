a7777d6ce4cc81a25c100a4bb70e0c2c431dcb2f
cmchaol <cmchaol@users.noreply.github.com>
2017-08-24 08:59:11 +0800

    prepare for llvm emerge

--- build.sh
+++ build.sh
@@ -4,4 +4,16 @@ mkdir /dev/tmp
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 
 emerge \
-       sys-devel/llvm
+       net-misc/proxychains \
+       net-misc/dhcpcd \
+       net-misc/autossh \
+       net-misc/keychain \
+       sys-boot/grub \
+       app-misc/mc \
+       dev-vcs/git
+       dev-util/re2c \
+       dev-util/ninja \
+       app-arch/libarchive \
+       net-misc/curl \
+       dev-libs/libuv \
+       dev-util/cmake
