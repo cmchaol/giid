c47ee46fbc0e242c44b4d38ab17d9e11d86caa34
cmchaol <cmchaol@users.noreply.github.com>
2017-08-24 11:57:02 +0800

    llvm alone

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM c5766/giid:v34
+FROM c5766/giid:v53
 # FROM gentoo/stage3-amd64-hardened
 
 ADD build.sh /
--- build.sh
+++ build.sh
@@ -1,19 +1,7 @@
 
 mkdir /dev/tmp
 
-echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
+# echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 
 emerge \
-       net-misc/proxychains \
-       net-misc/dhcpcd \
-       net-misc/autossh \
-       net-misc/keychain \
-       sys-boot/grub \
-       app-misc/mc \
-       dev-vcs/git \
-       dev-util/re2c \
-       dev-util/ninja \
-       app-arch/libarchive \
-       net-misc/curl \
-       dev-libs/libuv \
-       dev-util/cmake
+       sys-devel/llvm
