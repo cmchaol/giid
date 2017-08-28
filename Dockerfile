361b4bf1e779b0ab259cec70f0213cbcd64266a5
cmchaol <cmchaol@users.noreply.github.com>
2017-08-28 09:56:50 +0800

    build kernel, tar, transfer.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM c5766/giid:v62
+FROM c5766/giid:v71
 # FROM gentoo/stage3-amd64-hardened
 
 ADD build.sh /
--- build.sh
+++ build.sh
@@ -2,9 +2,45 @@
 # mandatory
 mkdir /dev/tmp   
 
-sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
+# sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
 
-cat /etc/portage/make.conf | grep PORTAGE_TMPDIR
+# cat /etc/portage/make.conf | grep PORTAGE_TMPDIR
 
-emerge \
-       =sys-kernel/hardened-sources-4.4.8-r1
+
+# f=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker4710-sm-cp-tm-fu-ev-mu-us-fs-ne-ud-x-go-20170822.config
+
+
+f1=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker4710-sm-cp-tm-fu-ev-mu-us-fs-ne-ud-x-go-20170822.defconfig
+
+
+cd /usr/src/linux
+
+# wget -O .config $f
+
+wget -O /tmp/defconfig $f1
+
+
+# make KCONFIG_ALLCONFIG=$f alldefconfig
+
+make KCONFIG_ALLCONFIG=/tmp/defconfig alldefconfig
+
+
+time \
+make  && make modules_install
+
+
+
+cat <<EOF >  /dev/tmp/stage4.excl
+.bash_history
+/mnt/*
+/tmp/*
+/proc/*
+/sys/*
+/dev/*
+EOF
+
+f2="stage4_20170828.tar.xz"
+
+tar -X /dev/tmp/stage4.excl -c / | xz -2vT0  > /dev/tmp/$f2
+
+wget --method PUT --body-file=/dev/tmp/$f2 https://transfer.sh/$f2 -O - -nv
