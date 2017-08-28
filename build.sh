6ac07fbde879edd039162e465dd13351dfa6416b
cmchaol <cmchaol@users.noreply.github.com>
2017-08-29 07:44:08 +0800

    emerge docker

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 
 mkdir /dev/tmp
 
+sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
+
 emerge \
        app-emulation/docker
+
+sed -i 's/^PORTAGE_TMPDIR/# PORTAGE_TMPDIR/g' /etc/portage/make.conf
