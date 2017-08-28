374d8a9fffbea25ade48799c71e5c73a2dcaa08d
cmchaol <cmchaol@users.noreply.github.com>
2017-08-28 17:29:52 +0800

    docker

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 
 mkdir /dev/tmp
 
-# echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
-
-
-sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
-
-emerge --autounmask-write www-client/google-chrome
-
-yes | etc-update --automode -3  
-
-emerge www-client/google-chrome
+emerge \
+       app-emulation/docker
