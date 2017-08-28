34c728472969c8c7c61452a67c84eba3cb1a9fba
cmchaol <cmchaol@users.noreply.github.com>
2017-08-28 08:16:17 +0800

    emerge  hardened-sources-4.4.8-r1

--- build.sh
+++ build.sh
@@ -1,28 +1,10 @@
 
-mkdir /dev/tmp
+# mandatory
+mkdir /dev/tmp   
 
-# echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
+sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
 
-echo "app-editors/emacs xft X jpeg png svg tiff gif imagemagick" > /etc/portage/package.use/emacs
+cat /etc/portage/make.conf | grep PORTAGE_TMPDIR
 
-emerge  \
-        dev-libs/lzo \
-        media-libs/libpng \
-        media-libs/giflib \
-        media-libs/tiff \
-        net-libs/liblockfile \
-        dev-libs/vala-common \
-        media-gfx/graphite2 \
-        sys-libs/binutils-libs \
-        app-eselect/eselect-ctags \
-        dev-lang/nasm \
-        x11-libs/cairo \
-        app-eselect/eselect-emacs \
-        media-gfx/imagemagick \
-        dev-libs/libcroco \
-        x11-libs/gdk-pixbuf \
-        media-libs/libjpeg-turbo \
-        media-libs/harfbuzz \
-        virtual/jpeg \
-        x11-libs/pango \
-        gnome-base/librsvg
+emerge \
+       =sys-kernel/hardened-sources-4.4.8-r1
