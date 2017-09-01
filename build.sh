653fbaa74627696957de633711959854dfc29b9e
cmchaol <cmchaol@users.noreply.github.com>
2017-09-01 17:56:13 +0800

    adjust commands sequence, make after configuration.

--- build.sh
+++ build.sh
@@ -20,10 +20,6 @@ emerge \
        =sys-kernel/hardened-sources-4.4.8-r1 
 
 
-time \
-make  && make modules_install
-
-
 
 f1=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker448-20170901.defconfig
 
@@ -31,5 +27,8 @@ cd /usr/src/linux
 wget -O /tmp/defconfig $f1
 make KCONFIG_ALLCONFIG=/tmp/defconfig alldefconfig
 
+
+time \
+make  && make modules_install
 
 sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf
