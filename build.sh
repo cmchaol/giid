8dc18da4b0cad5742168f3e3ba426050e808b92d
cmchaol <cmchaol@users.noreply.github.com>
2017-09-01 21:55:38 +0000

    tar, transfer.sh

--- build.sh
+++ build.sh
@@ -6,29 +6,20 @@ echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 # sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
 
 
-echo "sys-kernel/hardened-sources symlink" >> /etc/portage/package.use/hardened-sources
+cat <<EOF >  /dev/tmp/stage4.excl
+.bash_history
+/mnt/*
+/tmp/*
+/proc/*
+/sys/*
+/dev/*
+EOF
 
+f2="stage4_20170901.tar.xz"
 
-emerge \
-       =sys-kernel/hardened-sources-4.4.8-r1 \
-       --autounmask-write 
+tar -X /dev/tmp/stage4.excl -c / | xz -2vT0  > /dev/tmp/$f2
 
-yes | etc-update --automode -3  
+wget --method PUT --body-file=/dev/tmp/$f2 https://transfer.sh/$f2 -O - -nv
 
-
-emerge \
-       =sys-kernel/hardened-sources-4.4.8-r1 
-
-
-
-f1=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker448-20170901.defconfig
-
-cd /usr/src/linux
-wget -O /tmp/defconfig $f1
-make KCONFIG_ALLCONFIG=/tmp/defconfig alldefconfig
-
-
-time \
-make  && make modules_install
 
 sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf
