b9eb50e0a2b1ca4b6521a3043a2ad4d6fbf83c27
cmchaol <cmchaol@users.noreply.github.com>
2017-09-01 10:06:53 +0800

    boot, kernel, transfer.fs

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ mkdir /dev/tmp
 
 mkdir /usr/portage
 
-
-echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
-# sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
+# to accelerate portage building in tmpfs
+echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf  
 
 
 echo "sys-kernel/hardened-sources symlink" >> /etc/portage/package.use/hardened-sources
@@ -23,10 +22,10 @@ emerge \
        net-misc/keychain \
        app-misc/mc \
        dev-vcs/git \
-       sys-fs/squashfs-tools 
-
-
-#       =sys-kernel/hardened-sources-4.4.8-r1 \
+       sys-fs/squashfs-tools \
+       net-fs/cifs-utils \
+       sys-fs/squashfs-tools \
+       =sys-kernel/hardened-sources-4.4.8-r1 
 
 
 cat <<EOF >  /etc/conf.d/net
@@ -36,4 +35,32 @@ EOF
 
 emerge --noreplace net-misc/netifrc
 
-sed -i 's/^PORTAGE_TMPDIR/# PORTAGE_TMPDIR/g' /etc/portage/make.conf
+
+f1=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker448-20170901.defconfig
+
+
+cd /usr/src/linux
+wget -O /tmp/defconfig $f1
+make KCONFIG_ALLCONFIG=/tmp/defconfig alldefconfig
+
+
+time \
+make  && make modules_install
+
+cat <<EOF >  /dev/tmp/stage4.excl
+.bash_history
+/mnt/*
+/tmp/*
+/proc/*
+/sys/*
+/dev/*
+EOF
+
+f2="stage4_20170901.tar.xz"
+
+tar -X /dev/tmp/stage4.excl -c / | xz -2vT0  > /dev/tmp/$f2
+
+wget --method PUT --body-file=/dev/tmp/$f2 https://transfer.sh/$f2 -O - -nv
+
+
+sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf # restore to default
