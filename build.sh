bd4fd2a951ebcc3875c98c7f3776b0b3eb18736e
cmchaol <cmchaol@users.noreply.github.com>
2017-09-01 16:27:54 +0800

    from b19, add kernel and build kernel

--- build.sh
+++ build.sh
@@ -2,33 +2,22 @@
 mkdir /dev/tmp
 
 
-mkdir /usr/portage
-
-
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 # sed -i 's/# PORTAGE_TMPDIR/PORTAGE_TMPDIR/g' /etc/portage/make.conf 
 
 
-emerge-webrsync
+echo "sys-kernel/hardened-sources symlink" >> /etc/portage/package.use/hardened-sources
 
 
 emerge \
-       sys-boot/grub \
-       net-misc/proxychains \
-       net-misc/dhcpcd \
-       net-misc/autossh \
-       net-misc/keychain \
-       app-misc/mc \
-       dev-vcs/git \
-       sys-fs/squashfs-tools \
-       net-fs/curlftpfs 
+       =sys-kernel/hardened-sources-4.4.8-r1 
 
 
-cat <<EOF >  /etc/conf.d/net
-dns_domain_lo="my_domain"
-config_enp1s0="dhcp"
-EOF
+f1=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker448-20170901.defconfig
 
-emerge --noreplace net-misc/netifrc
+cd /usr/src/linux
+wget -O /tmp/defconfig $f1
+make KCONFIG_ALLCONFIG=/tmp/defconfig alldefconfig
+
 
 sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf
