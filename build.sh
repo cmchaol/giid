9bc2d93bb8c86e4179751b90e2b7b5728a98a7fa
cmchaol <cmchaol@users.noreply.github.com>
2017-09-07 17:05:51 +0800

    prepare for llvm

--- build.sh
+++ build.sh
@@ -9,18 +9,13 @@ echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 
 time \
 emerge \
-       sys-boot/grub \
-       net-misc/proxychains \
-       net-misc/dhcpcd \
-       net-misc/autossh \
-       net-misc/keychain \
-       app-misc/mc \
-       dev-vcs/git \
-       sys-fs/squashfs-tools \
-       net-fs/curlftpfs
+       dev-util/re2c \
+       dev-util/ninja \
+       app-arch/libarchive \
+       dev-libs/libuv \
+       dev-util/cmake 
 
 
+sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf 
 
-sed -i 's/^PORTAGE_TMPDIR/# PORTAGE_TMPDIR/g' /etc/portage/make.conf 
-
-rm -rf /usr/portage
+rm -rf /dev/tmp /usr/portage
