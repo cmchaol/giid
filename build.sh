2eb814894c420c21b5119d7d25cea5176db0ccbc
cmchaol <cmchaol@users.noreply.github.com>
2017-09-07 17:26:59 +0800

    llvm @ docker hub

--- build.sh
+++ build.sh
@@ -8,13 +8,8 @@ emerge-webrsync
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 
 time \
-emerge \
-       dev-util/re2c \
-       dev-util/ninja \
-       app-arch/libarchive \
-       dev-libs/libuv \
-       dev-util/cmake 
-
+emerge \ 
+       emerge sys-devel/llvm 
 
 sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf 
 
