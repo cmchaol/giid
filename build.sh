57daddd4983967e1e6edb6aca68526138eb6757c
cmchaol <cmchaol@users.noreply.github.com>
2017-09-08 10:47:48 +0800

    time xorg-drivers

--- build.sh
+++ build.sh
@@ -7,16 +7,24 @@ emerge-webrsync
 
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 
+sed -i 's/USE="/USE="udev xattr /' /etc/portage/make.conf 
 
+cat <<EOF >> /etc/portage/make.conf
 
-time\
+INPUT_DEVICES="libinput"
+
+VIDEO_CARDS="nouveau intel i915"
+
+PAX_MARKINGS="XT" 
+
+EOF
+
+time \
+emerge --changed-use --deep @world 
+
+time \
 emerge \
-       x11-base/xorg-server \
-       x11-apps/xrandr \
-       x11-terms/xterm \
-       x11-wm/spectrwm 
-
-
+       x11-base/xorg-drivers 
 
 
 sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf 
