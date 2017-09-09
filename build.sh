3c8b9b6d009d3ca87d08067d5b7f52f0170c2b28
cmchaol <cmchaol@users.noreply.github.com>
2017-09-09 11:49:15 +0800

    parpare for llvm

--- build.sh
+++ build.sh
@@ -1,47 +1,18 @@
 #!/bin/bash
 
-mkdir /dev/tmp /usr/portage
-sed -i 's/USE="/USE="udev xattr /' /etc/portage/make.conf 
+mkdir /dev/tmp 
+# sed -i 's/USE="/USE="udev xattr mmx sse sse2/' /etc/portage/make.conf 
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 echo FEATURES="buildpkg" >> /etc/portage/make.conf
 
-emerge-webrsync
-
-time \
-emerge --changed-use --deep @world  
-
-echo "sys-kernel/hardened-sources symlink" >> /etc/portage/package.use/hardened-sources
-
-emerge \
-       =sys-kernel/hardened-sources-4.4.8-r1 \
-       --autounmask-write 
-
-yes | etc-update --automode -3  
-
-emerge \
-       =sys-kernel/hardened-sources-4.4.8-r1 
-
-f1=https://raw.githubusercontent.com/cmchaol/gimw/master/my-kernel-defconfig/ker448-20170904.defconfig
-
-cd /usr/src/linux
-wget -O /tmp/defconfig $f1
-make KCONFIG_ALLCONFIG=/tmp/defconfig alldefconfig
-
-time \
-make  && make modules_install
-
 
 time \
 emerge \
-       sys-boot/grub \
-       net-misc/proxychains \
-       net-misc/dhcpcd \
-       net-misc/autossh \
-       net-misc/keychain \
-       app-misc/mc \
-       dev-vcs/git \
-       sys-fs/squashfs-tools \
-       net-fs/curlftpfs
+       dev-util/re2c \
+       dev-util/ninja \
+       dev-libs/libuv \
+       app-arch/libarchive \
+       dev-util/cmake 
 
 
 # sed -i '/USE=/d' /etc/portage/make.conf 
