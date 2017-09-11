755e2557befc876037fdbeed48d32a0d94fabd47
cmchaol <cmchaol@users.noreply.github.com>
2017-09-11 16:10:39 +0800

    emerge google chrome

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM c5766/giid:x21
+FROM c5766/giid:e12
 # FROM gentoo/stage3-amd64-hardened
 
 ADD build.sh /
--- build.sh
+++ build.sh
@@ -5,30 +5,13 @@ mkdir /dev/tmp
 echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
 echo FEATURES="buildpkg" >> /etc/portage/make.conf
 
-# echo "app-editors/emacs xft X jpeg png svg tiff gif imagemagick" > /etc/portage/package.use/emacs
+
+emerge --autounmask-write www-client/google-chrome
+
+yes | etc-update --automode -3  
 
 time \
-emerge  \
-        dev-libs/lzo \
-        media-libs/libpng \
-        media-libs/giflib \
-        media-libs/tiff \
-        net-libs/liblockfile \
-        dev-libs/vala-common \
-        media-gfx/graphite2 \
-        sys-libs/binutils-libs \
-        app-eselect/eselect-ctags \
-        dev-lang/nasm \
-        x11-libs/cairo \
-        app-eselect/eselect-emacs \
-        media-gfx/imagemagick \
-        dev-libs/libcroco \
-        x11-libs/gdk-pixbuf \
-        media-libs/libjpeg-turbo \
-        media-libs/harfbuzz \
-        virtual/jpeg \
-        x11-libs/pango \
-        gnome-base/librsvg 
+emerge www-client/google-chrome
 
 
 # sed -i '/USE=/d' /etc/portage/make.conf 
