1d837b800e7498b30e1a386e5e3507f9443d3d06
cmchaol <cmchaol@users.noreply.github.com>
2017-09-11 10:09:46 +0800

    prepare for emacs

--- build.sh
+++ build.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-mkdir /dev/tmp
+mkdir /dev/tmp 
+# sed -i 's/USE="/USE="udev xattr mmx sse sse2/' /etc/portage/make.conf 
+echo PORTAGE_TMPDIR=\"/dev/tmp\" >> /etc/portage/make.conf
+echo FEATURES="buildpkg" >> /etc/portage/make.conf
 
-cat <<EOF >  /dev/tmp/stage4.excl
-.bash_history
-/mnt/*
-/tmp/*
-/proc/*
-/sys/*
-/dev/*
-EOF
+# echo "app-editors/emacs xft X jpeg png svg tiff gif imagemagick" > /etc/portage/package.use/emacs
 
-f="x21_20170910.tar.xz"
+time \
+emerge  \
+        dev-libs/lzo \
+        media-libs/libpng \
+        media-libs/giflib \
+        media-libs/tiff \
+        net-libs/liblockfile \
+        dev-libs/vala-common \
+        media-gfx/graphite2 \
+        sys-libs/binutils-libs \
+        app-eselect/eselect-ctags \
+        dev-lang/nasm \
+        x11-libs/cairo \
+        app-eselect/eselect-emacs \
+        media-gfx/imagemagick \
+        dev-libs/libcroco \
+        x11-libs/gdk-pixbuf \
+        media-libs/libjpeg-turbo \
+        media-libs/harfbuzz \
+        virtual/jpeg \
+        x11-libs/pango \
+        gnome-base/librsvg 
 
-tar -X /dev/tmp/stage4.excl -c / | xz -2vT0  > /dev/tmp/$f
 
-wget --method PUT --body-file=/dev/tmp/$f https://transfer.sh/$f -O - -nv
+# sed -i '/USE=/d' /etc/portage/make.conf 
+sed -i '/PORTAGE_TMPDIR/d' /etc/portage/make.conf 
+sed -i '/FEATURES/d' /etc/portage/make.conf
