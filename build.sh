62afd8d6fd1a0336fbe2075bcc72118b064477da
cmchaol <cmchaol@users.noreply.github.com>
2017-09-13 09:21:50 +0800

    exclude usr/portage usr/src

--- build.sh
+++ build.sh
@@ -9,9 +9,11 @@ cat <<EOF >  /dev/tmp/stage4.excl
 /proc/*
 /sys/*
 /dev/*
+/usr/portage/*
+/usr/src/*
 EOF
 
-f="q12_20170912.tar.xz"
+f="q12_20170913.tar.xz"
 
 tar -X /dev/tmp/stage4.excl -c / | xz -2vT0  > /dev/tmp/$f
 
